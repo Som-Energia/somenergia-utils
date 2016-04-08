b652e6e5d1acb10b31e34287c965d99742f94ccb
César López Ramírez <cesar.lopez@somenergia.coop>
2016-04-08 09:02:01 +0200

    activate_wrapper added with doc

---
+++ README.md
@@ -0,0 +1,13 @@
+# somutils
+
+Different tools that you can use in your Python developing:
+
+* activate\_wrapper.sh
+
+## activate\_wrapper.sh
+
+You have to set an environment variable VIRTUALENV\_PATH as the folder where the VIRTUALENV is located.
+```
+usage: activate_wrapper.sh COMMAND
+```
+
---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+from setuptools import setup
+
+readme = open("README.rst").read()
+
+setup(
+	name = "somutils",
+	version = "1.0",
+	description = "Tools we use at Somenergia and can be useful",
+	author = "César López Ramírez",
+	author_email = "cesar.lopez@somenergia.coop",
+	url = 'https://github.com/Som-Energia/somenergia-utils',
+	long_description = readme,
+	license = 'GNU General Public License v3 or later (GPLv3+)',
+    py_modules = [
+        "somutils"
+        ],
+	scripts=[
+        'activate_wrapper.sh',
+		],
+	install_requires=[
+	],
+	classifiers = [
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2',
+		'Environment :: Console',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		'Intended Audience :: Developers',
+		'Development Status :: 5 - Production/Stable',
+		'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+		'Operating System :: OS Independent',
+	],
+)
+
