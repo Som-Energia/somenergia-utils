b851ee68ce83468d9cf1afd5a804efac2cd5042d
David García Garzón <david.garcia@somenergia.coop>
2021-02-25 16:16:20 +0100

    trace: py3 compatible print

--- somutils/trace.py
+++ somutils/trace.py
@@ -8,9 +8,9 @@ def trace(function, *args) :
     """Decorates a function by tracing the begining and
     end of the function execution, if doTrace global is True"""
 
-    if doTrace : print ("> "+function.__name__, args)
+    if doTrace : print ("> {} {}".format(function.__name__, args))
     result = function(*args)
-    if doTrace : print ("< "+function.__name__, args, "->", result)
+    if doTrace : print ("< {} {} -> {}".format(function.__name__, args, result))
     return result
 
 # vim: ts=4 sw=4 et
