d9f9a1fae02da15f1a891c9f8f43cae181d6f2ba
David García Garzón <david.garcia@somenergia.coop>
2022-04-11 01:02:43 +0200

    erptree: helper function testing

--- somutils/erptree.py
+++ somutils/erptree.py
@@ -28,6 +28,10 @@ def erptree(
     """
 
     def processAttributes(attributes):
+        """For each dotted attribute provides the containing
+        parent object the name within the parent and the original
+        full dotted name.
+        """
         for attribute in listify(attributes):
             for context, leaf in  getContext(target, attribute):
                 yield context, leaf, attribute
@@ -68,37 +72,103 @@ def erptree(
 
 from contextlib import contextmanager
 
+
 @contextmanager
 def step(doing, attribute):
-    print(doing, attribute)
+    #print(doing, attribute)
     try:
         yield
     except Exception as e:
         print("Error while", doing, attribute)
         raise
 
+
 def listify(attributes):
-    if not attributes: return []
+    """
+    If the param evals to false, it returns an empty list.
+    If the param is a string, it returns it splited and sorted.
+    Else it takes the param as an iterable and returns it sorted.
+
+    >>> listify('element')
+    ['element']
+    >>> listify('element2 element1')
+    ['element1', 'element2']
+    >>> listify(['element2', 'element1'])
+    ['element1', 'element2']
+    >>> listify([])
+    []
+    >>> listify(None)
+    []
+    """
+
+    if not attributes:
+        return []
     if type(attributes) == str:
         return sorted(attributes.split())
     return sorted(attributes)
 
+
 def getContext(o, attribute):
-    steps = attribute.split('.')
-    if len(steps) == 1:
-        yield o, attribute
-        return
-        for item in o:
-            yield item, attribute
-        return
+    """
+    Given the structure `o`, and a dotted attribute path,
+    returns the inmediate parents having the attribute and the leave attribute name.
+
+    So given the structure:
+
+    >>> data = ns.loads('''
+    ...   attrib1:
+    ...     attrib11: value11
+    ...     attrib12: value12
+    ...   attrib2:
+    ...     - attrib21: valueItem0
+    ...     - attrib21: valueItem1
+    ...   attrib3:
+    ...     - attrib31:
+    ...         attrib311: valueItem0
+    ...     - attrib31:
+    ...         attrib311: valueItem1
+    ... ''')
+
+    You get:
+
+    >>> list(getContext(data, 'attrib1')) == [(data, 'attrib1')]
+    True
+    >>> list(getContext(data.attrib1, 'attrib11')) == [(data.attrib1, 'attrib11')]
+    True
+    >>> list(getContext(data, 'attrib1.attrib11')) == [(data.attrib1, 'attrib11')]
+    True
+    >>> list(getContext(data.attrib2, 'attrib21')) ==  [
+    ...     (data.attrib2[0], 'attrib21'),
+    ...     (data.attrib2[1], 'attrib21'),
+    ... ]
+    True
+    >>> list(getContext(data, 'attrib2.attrib21')) == [
+    ...     (data.attrib2[0], 'attrib21'),
+    ...     (data.attrib2[1], 'attrib21'),
+    ... ]
+    True
+    >>> list(getContext(data, 'attrib3.attrib31.attrib311')) == [
+    ...     (data.attrib3[0].attrib31, 'attrib311'),
+    ...     (data.attrib3[1].attrib31, 'attrib311'),
+    ... ]
+    True
+    """
+
     if type(o) == list:
         for item in o:
             # TODO: substitute by "yield from" on Py2 dropped
-            for x in getContext(item, attribute): yield x
+            for x in getContext(item, attribute):
+                yield x
         return
-    nextSteps = '.'.join(steps[1:])
+
+    steps = attribute.split('.', 1)
+    thisStep, remaining = steps[0], steps[1:]
+    if not remaining:
+        yield o, thisStep
+        return
+
     # TODO: substitute by "yield from" on Py2 dropped
-    for x in getContext(o[steps[0]], nextSteps):
+    for x in getContext(o[thisStep], remaining[0]):
         yield x
 
 
